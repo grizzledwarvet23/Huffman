549056284f9b2643af910ee446fa05902c40bf76
grizzledwarvet23 <72232563+grizzledwarvet23@users.noreply.github.com>
2022-11-29 23:31:15 -0600

    gathered empirical data

--- src/SimpleHuffProcessor.java
+++ src/SimpleHuffProcessor.java
@@ -24,6 +24,143 @@ import java.sql.SQLOutput;
 import java.util.HashMap;
 import java.util.TreeMap;
 
+/** EMPIRICAL DATA:
+ *
+ * calgary -
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\calgary\calgary\bib.hf
+ * Reached
+ * bib from	 111261 to	 73795 in	 0.420
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\calgary\calgary\book1.hf
+ * Reached
+ * book1 from	 768771 to	 439409 in	 1.975
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\calgary\calgary\book2.hf
+ * Reached
+ * book2 from	 610856 to	 369335 in	 1.295
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\calgary\calgary\geo.hf
+ * Reached
+ * geo from	 102400 to	 73592 in	 0.221
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\calgary\calgary\news.hf
+ * Reached
+ * news from	 377109 to	 247428 in	 0.723
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\calgary\calgary\obj1.hf
+ * Reached
+ * obj1 from	 21504 to	 17085 in	 0.051
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\calgary\calgary\obj2.hf
+ * Reached
+ * obj2 from	 246814 to	 195131 in	 0.569
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\calgary\calgary\paper1.hf
+ * Reached
+ * paper1 from	 53161 to	 34371 in	 0.101
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\calgary\calgary\paper2.hf
+ * Reached
+ * paper2 from	 82199 to	 48649 in	 0.147
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\calgary\calgary\paper3.hf
+ * Reached
+ * paper3 from	 46526 to	 28309 in	 0.086
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\calgary\calgary\paper4.hf
+ * Reached
+ * paper4 from	 13286 to	 8894 in	 0.031
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\calgary\calgary\paper5.hf
+ * Reached
+ * paper5 from	 11954 to	 8465 in	 0.029
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\calgary\calgary\paper6.hf
+ * Reached
+ * paper6 from	 38105 to	 25057 in	 0.074
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\calgary\calgary\pic.hf
+ * Reached
+ * pic from	 513216 to	 107586 in	 0.335
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\calgary\calgary\progc.hf
+ * Reached
+ * progc from	 39611 to	 26948 in	 0.080
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\calgary\calgary\progl.hf
+ * Reached
+ * progl from	 71646 to	 44017 in	 0.137
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\calgary\calgary\progp.hf
+ * Reached
+ * progp from	 49379 to	 31248 in	 0.101
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\calgary\calgary\trans.hf
+ * Reached
+ * trans from	 93695 to	 66252 in	 0.195
+ * --------
+ * total bytes read: 3251493
+ * total compressed bytes 1845571
+ * total percent compression 43.239
+ * compression time: 6.570
+ *
+ * BooksAndHTML -
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\BooksAndHTML\BooksAndHTML\A7_Recursion.html.hf
+ * Reached
+ * A7_Recursion.html from	 41163 to	 26189 in	 0.172
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\BooksAndHTML\BooksAndHTML\CiaFactBook2000.txt.hf
+ * Reached
+ * CiaFactBook2000.txt from	 3497369 to	 2260664 in	 6.766
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\BooksAndHTML\BooksAndHTML\jnglb10.txt.hf
+ * Reached
+ * jnglb10.txt from	 292059 to	 168618 in	 0.524
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\BooksAndHTML\BooksAndHTML\kjv10.txt.hf
+ * Reached
+ * kjv10.txt from	 4345020 to	 2489768 in	 7.412
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\BooksAndHTML\BooksAndHTML\melville.txt.hf
+ * Reached
+ * melville.txt from	 82140 to	 47364 in	 0.177
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\BooksAndHTML\BooksAndHTML\quotes.htm.hf
+ * Reached
+ * quotes.htm from	 61563 to	 38423 in	 0.132
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\BooksAndHTML\BooksAndHTML\rawMovieGross.txt.hf
+ * Reached
+ * rawMovieGross.txt from	 117272 to	 53833 in	 0.166
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\BooksAndHTML\BooksAndHTML\revDictionary.txt.hf
+ * Reached
+ * revDictionary.txt from	 1130523 to	 611618 in	 2.013
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\BooksAndHTML\BooksAndHTML\syllabus.htm.hf
+ * Reached
+ * syllabus.htm from	 33273 to	 21342 in	 0.072
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\BooksAndHTML\BooksAndHTML\ThroughTheLookingGlass.txt.hf
+ * Reached
+ * ThroughTheLookingGlass.txt from	 188199 to	 110293 in	 0.413
+ * --------
+ * total bytes read: 9788581
+ * total compressed bytes 5828112
+ * total percent compression 40.460
+ * compression time: 17.847
+ *
+ * waterloo -
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\waterloo\waterloo\clegg.tif.hf
+ * Reached
+ * clegg.tif from	 2149096 to	 2034595 in	 6.252
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\waterloo\waterloo\frymire.tif.hf
+ * Reached
+ * frymire.tif from	 3706306 to	 2188593 in	 6.191
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\waterloo\waterloo\lena.tif.hf
+ * Reached
+ * lena.tif from	 786568 to	 766146 in	 2.161
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\waterloo\waterloo\monarch.tif.hf
+ * Reached
+ * monarch.tif from	 1179784 to	 1109973 in	 3.101
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\waterloo\waterloo\peppers.tif.hf
+ * Reached
+ * peppers.tif from	 786568 to	 756968 in	 2.142
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\waterloo\waterloo\sail.tif.hf
+ * Reached
+ * sail.tif from	 1179784 to	 1085501 in	 3.069
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\waterloo\waterloo\serrano.tif.hf
+ * Reached
+ * serrano.tif from	 1498414 to	 1127645 in	 4.020
+ * compressing to: C:\Users\zayaa\IdeaProjects\Assignment10\waterloo\waterloo\tulips.tif.hf
+ * Reached
+ * tulips.tif from	 1179784 to	 1135861 in	 3.481
+ * --------
+ * total bytes read: 12466304
+ * total compressed bytes 10205282
+ * total percent compression 18.137
+ * compression time: 30.417
+ *
+ *
+ *
+ *
+ *
+ */
+
 public class SimpleHuffProcessor implements IHuffProcessor {
 
     private IHuffViewer myViewer;
@@ -63,13 +200,13 @@ public class SimpleHuffProcessor implements IHuffProcessor {
         counter.buildQueue();
         counter.buildTree();
         counter.mapChunkToCodes();
-        myViewer.update(counter.frequencies.toString());
+     //   myViewer.update(counter.frequencies.toString());
 
         header = headerFormat;
 
         //original size: count sum of (frequencies * 8)
         //new size: count sum of (frequencies * string length of new shiz)
-        myViewer.update(counter.chunkCodes.toString());
+   //     myViewer.update(counter.chunkCodes.toString());
 
         for (Integer key : counter.frequencies.keySet()) {
             oldSize += (BITS_PER_WORD * counter.frequencies.get(key));
@@ -89,8 +226,8 @@ public class SimpleHuffProcessor implements IHuffProcessor {
             newSize += (counter.leaves * 10) + counter.internalNodes + BITS_PER_INT;
         }
 
-        myViewer.update("Old Size: " + oldSize);
-        myViewer.update("New Size: " + newSize);
+   //     myViewer.update("Old Size: " + oldSize);
+   //     myViewer.update("New Size: " + newSize);
 
         return oldSize - newSize;
     }
@@ -147,7 +284,7 @@ public class SimpleHuffProcessor implements IHuffProcessor {
 
             outBitStream.close();
 
-            myViewer.showMessage("Bits of the new file: " + newSize);
+        //    myViewer.showMessage("Bits of the new file: " + newSize);
 
             return newSize;
         }
@@ -217,8 +354,7 @@ public class SimpleHuffProcessor implements IHuffProcessor {
                 huffTree.buildQueue();
                 huffTree.buildTree();
                 treeRoot = huffTree.tree;
-                myViewer.update(huffTree.frequencies.toString());
-
+          //      myViewer.update(huffTree.frequencies.toString());
             } else if (headerFormat == STORE_TREE) {
                 inStream.readBits(BITS_PER_INT);
                 treeRoot = reconstructTree(inStream);
